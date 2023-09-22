c3824eaab7d354442b4e1a8388328f56352817f6
rcanfield <Rcanfield86@gmail.com>
2023-09-21 23:12:56 -0400

    NX example repo

---
+++ apps/mfe-monorepo/src/app/features/Home/components/Home.tsx
@@ -0,0 +1,24 @@
+import { ProductsOnSale } from '../../Products';
+import styles from './Home.module.css';
+import useScreenSize from '../../Products/hooks/useScreenSize';
+import { Suspense } from 'react';
+import React from 'react';
+
+const ProductHero = React.lazy(() => import('hero/ProductHero'));
+
+const bodyElement = document.querySelector('body')!;
+
+const Home = () => {
+  const { isSmallScreen } = useScreenSize({
+    htmlElement: bodyElement,
+  });
+
+  return (
+    <section className={styles['home']}>
+      <Suspense>{!isSmallScreen && <ProductHero />}</Suspense>
+      <ProductsOnSale />
+    </section>
+  );
+};
+
+export default Home;
--- host/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-const appElement = document.getElementById("app");
-
-const root = createRoot(appElement!);
-root.render(<App />);
