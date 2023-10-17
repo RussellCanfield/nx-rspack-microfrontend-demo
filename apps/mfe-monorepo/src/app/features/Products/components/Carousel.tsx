fb32099f331904af5f56bff976ef051a3ea1bfde
rcanfield <Rcanfield86@gmail.com>
2023-10-16 22:08:30 -0400

    Remove redundant attributes

--- apps/mfe-monorepo/src/app/features/Products/components/Carousel.tsx
+++ apps/mfe-monorepo/src/app/features/Products/components/Carousel.tsx
@@ -1,31 +1,24 @@
-import styles from "./Carousel.module.css";
-import { ProductSaleData } from "../../../data/ProductData";
-import ProductCard from "./ProductCard";
-import { useCart } from "../../../hooks";
+import styles from './Carousel.module.css';
+import { ProductSaleData } from '../../../data/ProductData';
+import ProductCard from './ProductCard';
+import { useCart } from '../../../hooks';
 
 const ProductCarousel = () => {
-	const { addToCart } = useCart();
+  const { addToCart } = useCart();
 
-	return (
-		<ul className={styles["product-carousel"]}>
-			{ProductSaleData.map((product) => (
-				<li
-					key={product.id}
-					className={styles["product-carousel__item"]}
-				>
-					<ProductCard product={product}>
-						<button
-							type="button"
-							role="button"
-							onClick={() => addToCart(product)}
-						>
-							Add to Cart
-						</button>
-					</ProductCard>
-				</li>
-			))}
-		</ul>
-	);
+  return (
+    <ul className={styles['product-carousel']}>
+      {ProductSaleData.map((product) => (
+        <li key={product.id} className={styles['product-carousel__item']}>
+          <ProductCard product={product}>
+            <button type="button" onClick={() => addToCart(product)}>
+              Add to Cart
+            </button>
+          </ProductCard>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ProductCarousel;
